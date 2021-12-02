ac24306a87702614088808ed3e6618b59d5a03a1
Eduardo Zamin <eduardozamin@users.noreply.github.com>
2021-12-02 19:15:25 +0100

    Day 01

---
+++ e01/solution-e01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+with open("input-e01") as f:
+    data = f.read()
+
+all_numbers = [int(x) for x in data.split()]
+arr1 = np.array(all_numbers[:-1])
+arr2 = np.array(all_numbers[1:])
+
+arr_diff = arr2 - arr1
+
+results = [x for x in arr_diff if x > 0]
+
+print(len(results))
